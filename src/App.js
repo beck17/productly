4f8186cee8c462f790ee0f44e471ab15f260f54b
Bogdan Odintsov <bacha1122001@gmail.com>
2022-07-14 08:49:45 +0300

    Productly

--- src/App.js
+++ src/App.js
@@ -1,24 +1,449 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <div className="container">
+          <div className="header__inner">
+            <a className="logo" href="#">
+              <img src="images/logo.svg" alt="" />
+            </a>
+            <nav className="menu">
+              <div className="menu__btn">
+                <span></span>
+              </div>
+              <ul className="menu__list">
+                <li className="menu__list-item">
+                  <a className="menu__list-link" href="#">
+                    Product
+                  </a>
+                </li>
+                <li className="menu__list-item">
+                  <a className="menu__list-link" href="#">
+                    Customers
+                  </a>
+                </li>
+                <li className="menu__list-item">
+                  <a className="menu__list-link" href="#">
+                    Pricing
+                  </a>
+                </li>
+                <li className="menu__list-item">
+                  <a className="menu__list-link" href="#">
+                    Resources
+                  </a>
+                </li>
+              </ul>
+            </nav>
+            <div className="user-nav">
+              <a className="user-nav__link" href="#">Sign In</a>
+              <a className="user-nav__link" href="#">Sign Up</a>
+            </div>
+          </div>
+        </div>
       </header>
-    </div>
+
+      <section className="top">
+        <div className="container">
+          <div className="top__inner">
+            <h1 className="top__title">
+              The Design Thinking superpowers
+            </h1>
+            <p className="top__text">
+              Tools, tutorials, design and innovation experts,
+              all in one place! The most intuitive way to imagine
+              your next user experience.
+            </p>
+            <button className="top__btn">
+              Get started
+            </button>
+            <a className="top__link" href="#">
+              Watch the Video
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <section className="tools">
+        <h3 className="tools__title title">
+          We design tools to unveil your superpowers
+        </h3>
+        <div className="tools__inner">
+          <div className="container">
+            <div className="tools__items">
+
+              <div className="tools__item">
+                <img className="tools__item-img " src="images/icon-1.svg" alt="" />
+                  <h5 className="tools__item-title title">
+                    First click tests
+                  </h5>
+                  <p className="tools__item-text">
+                    While most people enjoy casino gambling.
+                  </p>
+              </div>
+
+              <div className="tools__item">
+                <img className="tools__item-img " src="images/icon-2.svg" alt="" />
+                  <h5 className="tools__item-title title">
+                    Design surveys
+                  </h5>
+                  <p className="tools__item-text">
+                    Sports betting, lottery and bingo playing for the fun.
+                  </p>
+              </div>
+
+              <div className="tools__item">
+                <img className="tools__item-img" src="images/icon-3.svg" alt="" />
+                  <h5 className="tools__item-title title">
+                    Preference tests
+                  </h5>
+                  <p className="tools__item-text">
+                    The Myspace page defines the individual.
+                  </p>
+              </div>
+
+              <div className="tools__item">
+                <img className="tools__item-img" src="images/icon-4.svg" alt="" />
+                  <h5 className="tools__item-title title">
+                    Five second tests
+                  </h5>
+                  <p className="tools__item-text">
+                    Personal choices and the overall personality of the person.
+                  </p>
+              </div>
+            </div>
+          </div>
+          <a className="tools__link" href="#">SIGN UP NOW</a>
+        </div>
+      </section>
+
+      <section className="direction">
+        <div className="container">
+
+          <div className="direction__item">
+            <div className="direction__item-content">
+              <p className="direction__item-pretitle">
+                Effortless Validation for
+              </p>
+              <h3 className="direction__item-title title">
+                Design Professionals
+              </h3>
+              <p className="direction__item-text">
+                The Myspace page defines the individual, his or her
+                characteristics, traits, personal choices and the overall
+                personality of the person.
+              </p>
+              <h6 className="direction__item-subtitle title">
+                Accessory makers
+              </h6>
+              <p className="direction__item-text">
+                While most people enjoy casino gambling,
+                sports betting, lottery and bingo playing for the fun
+              </p>
+              <h6 className="direction__item-subtitle title">
+                Alterationists
+              </h6>
+              <p className="direction__item-text">
+                If you are looking for a new way to promote your
+                business that won’t cost you more money
+              </p>
+              <h6 className="direction__item-subtitle title">
+                Custom Design designers
+              </h6>
+              <p className="direction__item-text">
+                If you are looking for a new way to promote your
+                business that won’t cost you more money.
+              </p>
+            </div>
+            <img className="direction__item-img" src="images/direction-1.svg" alt="" />
+          </div>
+
+          <div className="direction__item">
+            <img className="direction__item-img" src="images/direction-2.svg" alt="" />
+              <div className="direction__item-content">
+                <p className="direction__item-pretitle">
+                  Easier decision making for
+                </p>
+                <h3 className="direction__item-title title">
+                  Product Managers
+                </h3>
+                <p className="direction__item-text">
+                  The Myspace page defines the individual, his or her
+                  characteristics, traits, personal choices and the overall
+                  personality of the person.
+                </p>
+                <ul className="direction__list">
+                  <li className="direction__list-item">
+                    Never worry about overpaying for your energy again.
+                  </li>
+                  <li className="direction__list-item">
+                    We will only switch you to energy companies that we
+                    trust and will treat you right
+                  </li>
+                  <li className="direction__list-item">
+                    We track the markets daily and know where the savings are.
+                  </li>
+                </ul>
+              </div>
+          </div>
+
+          <div className="direction__item">
+            <div className="direction__item-content">
+              <p className="direction__item-pretitle">
+                Optimisation for
+              </p>
+              <h3 className="direction__item-title title">
+                Marketers
+              </h3>
+              <p className="direction__item-text">
+                Few would argue that, despite the advancements of
+                feminism over the past three decades, women still face
+                a double standard when it comes to their behavior.
+              </p>
+              <h6 className="direction__item-subtitle title">
+                Accessory makers
+              </h6>
+              <p className="direction__item-text">
+                While most people enjoy casino gambling, sports betting,
+                lottery and bingo playing for the fun
+              </p>
+              <h6 className="direction__item-subtitle title">
+                Alterationists
+              </h6>
+              <p className="direction__item-text">
+                If you are looking for a new way to promote your
+                business that won’t cost you more money
+              </p>
+              <h6 className="direction__item-subtitle title">
+                Custom Design designers
+              </h6>
+              <p className="direction__item-text">
+                If you are looking for a new way to promote your
+                business that won’t cost you more money.
+              </p>
+            </div>
+            <img className="direction__item-img" src="images/direction-3.svg" alt="" />
+          </div>
+
+        </div>
+      </section>
+
+      <section className="team">
+        <div className="container">
+          <div className="team__inner">
+
+            <div className="team__item team__item--1">
+              <img className="team__item-img" src="images/team-1.jpg" alt="" />
+                <p className="team__item-text team__item-text--right">
+                  I can help marketing strategy
+                </p>
+            </div>
+
+            <div className="team__item team__item--2">
+              <img className="team__item-img" src="images/team-2.jpg" alt="" />
+                <p className="team__item-text team__item-text--left">
+                  I can take care of your pitch
+                </p>
+            </div>
+
+            <div className="team__item team__item--3">
+              <img className="team__item-img" src="images/team-3.jpg" alt="" />
+                <p className="team__item-text team__item-text--right">
+                  I can prototype your app
+                </p>
+            </div>
+
+            <div className="team__item team__item--4">
+              <img className="team__item-img" src="images/team-4.jpg" alt="" />
+                <p className="team__item-text team__item-text--right">
+                  I can design you website
+                </p>
+            </div>
+
+            <div className="team__item team__item--5">
+              <img className="team__item-img" src="images/team-5.jpg" alt="" />
+                <p className="team__item-text team__item-text--right">
+                  I will define the profile of your users
+                </p>
+            </div>
+
+            <div className="team__box">
+              <h3 className="team__title title">
+                Need a super hero?
+              </h3>
+              <p className="team__text">
+                Do you require some help for your project:
+                Conception workshop,prototyping, marketing
+                strategy, landing page, Ux/UI?
+              </p>
+              <button className="team__btn">
+                Contact our expert
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="blog">
+        <div className="container">
+
+          <h3 className="blog__title title">
+            Marketing Strategies
+          </h3>
+          <p className="blog__text">
+            Join 40,000+ other marketers and
+            get proven strategies on email marketing
+          </p>
+          <div className="slider">
+            <div className="slider__item">
+              <img className="slider__item-img" src="images/slide-1.svg" alt="" />
+                <div className="slider__item-info">
+                  By <a className="slider__item-author" href="#">
+                  Abdullah</a> | 03 March 2019
+                </div>
+                <a className="slider__item-title title" href="#">
+                  Increasing Prosperity With Positive Thinking
+                </a>
+            </div>
+            <div className="slider__item">
+              <img className="slider__item-img" src="images/slide-2.svg" alt="" />
+                <div className="slider__item-info">
+                  By <a className="slider__item-author" href="#">
+                  Abdullah</a> | 03 March 2019
+                </div>
+                <a className="slider__item-title title" href="#">
+                  Motivation Is The First Step To Success
+                </a>
+            </div>
+            <div className="slider__item">
+              <img className="slider__item-img" src="images/slide-3.svg" alt="" />
+                <div className="slider__item-info">
+                  By <a className="slider__item-author" href="#">
+                  Abdullah</a> | 03 March 2019
+                </div>
+                <a className="slider__item-title title" href="#">
+                  Success Steps For Your Personal Or Business Life
+                </a>
+            </div>
+
+            <div className="slider__item">
+              <img className="slider__item-img" src="images/slide-1.svg" alt="" />
+                <div className="slider__item-info">
+                  By <a className="slider__item-author" href="#">
+                  Abdullah</a> | 03 March 2019
+                </div>
+                <a className="slider__item-title title" href="#">
+                  Increasing Prosperity With Positive Thinking
+                </a>
+            </div>
+            <div className="slider__item">
+              <img className="slider__item-img" src="images/slide-2.svg" alt="" />
+                <div className="slider__item-info">
+                  By <a className="slider__item-author" href="#">
+                  Abdullah</a> | 03 March 2019
+                </div>
+                <a className="slider__item-title title" href="#">
+                  Motivation Is The First Step To Success
+                </a>
+            </div>
+            <div className="slider__item">
+              <img className="slider__item-img" src="images/slide-3.svg" alt="" />
+                <div className="slider__item-info">
+                  By <a className="slider__item-author" href="#">
+                  Abdullah</a> | 03 March 2019
+                </div>
+                <a className="slider__item-title title" href="#">
+                  Success Steps For Your Personal Or Business Life
+                </a>
+            </div>
+
+            <div className="slider__item">
+              <img className="slider__item-img" src="images/slide-1.svg" alt="" />
+                <div className="slider__item-info">
+                  By <a className="slider__item-author" href="#">
+                  Abdullah</a> | 03 March 2019
+                </div>
+                <a className="slider__item-title title" href="#">
+                  Increasing Prosperity With Positive Thinking
+                </a>
+            </div>
+            <div className="slider__item">
+              <img className="slider__item-img" src="images/slide-2.svg" alt="" />
+                <div className="slider__item-info">
+                  By <a className="slider__item-author" href="#">
+                  Abdullah</a> | 03 March 2019
+                </div>
+                <a className="slider__item-title title" href="#">
+                  Motivation Is The First Step To Success
+                </a>
+            </div>
+            <div className="slider__item">
+              <img className="slider__item-img" src="images/slide-3.svg" alt="" />
+                <div className="slider__item-info">
+                  By <a className="slider__item-author" href="#">
+                  Abdullah</a> | 03 March 2019
+                </div>
+                <a className="slider__item-title title" href="#">
+                  Success Steps For Your Personal Or Business Life
+                </a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer className="footer">
+        <div className="container">
+          <div className="footer__inner">
+            <a className="logo" href="#">
+              <img src="images/logo.svg" alt="" />
+            </a>
+            <div className="footer__menu">
+              <h6 className="footer__menu-title">Quick Links</h6>
+              <ul className="footer__list">
+                <li className="footer__menu-item">
+                  <a className="footer__menu-link" href="#">About Us</a>
+                </li>
+                <li className="footer__menu-item">
+                  <a className="footer__menu-link" href="#">Blog</a>
+                </li>
+                <li className="footer__menu-item">
+                  <a className="footer__menu-link" href="#">Contact</a>
+                </li>
+                <li className="footer__menu-item">
+                  <a className="footer__menu-link" href="#">FAQ</a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="footer__menu">
+              <h6 className="footer__menu-title">Legal Stuff</h6>
+              <ul className="footer__list">
+                <li className="footer__menu-item">
+                  <a className="footer__menu-link" href="#">Disclaimer</a>
+                </li>
+                <li className="footer__menu-item">
+                  <a className="footer__menu-link" href="#">Financing</a>
+                </li>
+                <li className="footer__menu-item">
+                  <a className="footer__menu-link" href="#">Privacy Policy</a>
+                </li>
+                <li className="footer__menu-item">
+                  <a className="footer__menu-link" href="#">Terms of Service</a>
+                </li>
+              </ul>
+            </div>
+
+            <form className="footer__form" action="#">
+              <p className="footer__form-text">
+                knowing you're always on the best energy deal.
+              </p>
+              <input className="footer__form-input" type="text" placeholder="Enter your phone Number " />
+                <button className="footer__form-btn" type="submit">Sign up Now</button>
+            </form>
+          </div>
+
+        </div>
+      </footer>
+    </>
   );
 }
 
